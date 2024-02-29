3fbc332f2439a7228963b7939fed30f275387920
IneptDiviner <145461276+IneptDiviner@users.noreply.github.com>
2024-02-29 10:14:38 -0500

    Started this, didn't complete it.

--- Exercise9/Exercise9/Program.cs
+++ Exercise9/Exercise9/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("please enter a row width");
+            int row = int.Parse(Console.ReadLine());
+            Console.WriteLine("Please enter a row quantinty");
+            int quantity = int.Parse(Console.ReadLine());
+            string ast = "*";
+            for (int countRowHeight = 0; countRowHeight < row; countRowHeight++)
+            {
+                Console.WriteLine(ast);
+                for (int countRowWidth = 1; countRowWidth < row; countRowWidth++)
+                { 
+                    Console.Write(ast);
+                }
+            }
         }
     }
 }
