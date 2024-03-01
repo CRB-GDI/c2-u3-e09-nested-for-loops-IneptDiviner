54043f1e7e84562ea75777639a28fee165326774
IneptDiviner <keith.wright@s.Capregboces.org>
2024-03-01 10:15:32 -0500

    still didn't do problem 1

--- Exercise9/Exercise9/Program.cs
+++ Exercise9/Exercise9/Program.cs
@@ -8,13 +8,13 @@
             int rows = int.Parse(Console.ReadLine());
             Console.WriteLine("Please enter a number of ");
             int columns = int.Parse(Console.ReadLine());
+            string ast = "*";
             Console.WriteLine(" | ");
             Console.WriteLine(" V ");
             Console.WriteLine(" ");
             for (int col = 1; col <= columns; col++)
             {
-                Console.Write("{0, 3}*");
-
+            
             }
 
             
